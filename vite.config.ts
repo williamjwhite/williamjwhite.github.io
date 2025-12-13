063b25c3ef9752f5679804cb886d21a5114f7714
William W. <69099196+Willj518@users.noreply.github.com>
2025-12-13 09:08:32 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,22 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
   server: {
-    host: true, // same as 0.0.0.0
+    host: true, // LAN + mobile access
     port: 5173,
     strictPort: true,
-    // If you proxy APIs locally, add proxy here
-    // proxy: { "/api": "http://localhost:3000" },
     open: true,
   },
 });
